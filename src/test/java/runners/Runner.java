2ca89096394bed82df7113ecac43f3c8b571ab64
reema <ralnakhebi@hotmail.com>
2024-04-16 15:33:07 +0300

    HW

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,2 +1,23 @@
-package runners;public class runner {
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "stepDefinitions",
+        features = "src/test/resources/features",
+        plugin = {
+                "html:target/html-report/report.html",
+                "json:target/json-report/report.json",
+                "junit:target/xml-report/report.xml",
+                "pretty"
+        },
+        dryRun = false
+)
+public class Runner {
+
 }
+
--- src/test/resources/features/register.feature
+++ src/test/resources/features/register.feature
@@ -2,4 +2,5 @@ Feature: Register New Account
   Scenario: enter a valid Credentials
     Given The page displayed
     When Enter valid Credentials and click continue button
-    And Verify the complete of the Registration
+    Then Verify the complete of the Registration
+    And close browser
